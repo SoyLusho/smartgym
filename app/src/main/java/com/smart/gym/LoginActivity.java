c1f2942ca7ff2d282751a1e453acf86080a2da50
“cristian” <clozada89@itfip.edu.co>
2019-04-12 12:14:10 -0500

    ahora la app implementa publicidad

--- app/src/main/java/com/smart/gym/LoginActivity.java
+++ app/src/main/java/com/smart/gym/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.smart.gym;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button entrar;
+    InterstitialAd interstitialAd; // Publicidad pantalla completa
+    AdView adView; //publicidad tipo Banner osea pequeñita
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         entrar=(Button)findViewById(R.id.button1);
         entrar.setOnClickListener(this);
+
+
+        //baner pequeño
+        adView = new AdView(this);
+        adView.setAdUnitId(getString(R.string.id_publicidad_banner));
+        adView.setAdSize(AdSize.BANNER);
+
+        CardView cardView = (CardView) findViewById(R.id.banner_publicidad);
+        AdRequest adRequest2 = new AdRequest.Builder().build();
+        adView.loadAd(adRequest2);
+        cardView.addView(adView);//esto carga el baner pequeño enla interfaz
+
+        //aqui va la de pantalla completa
+        AdRequest adRequest = new AdRequest.Builder().build();
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.id_publicidad_pantalla_completa));
+        AdRequest adRequest1 = new AdRequest.Builder().build();
+        interstitialAd.loadAd(adRequest);
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                interstitialAd.show();
+            }
+        });
+
+
 
     }
 
